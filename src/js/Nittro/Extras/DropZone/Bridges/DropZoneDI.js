e1aca6362fbaa86008fac667e091a355bddca606
danik <me@subsonic.cz>
2016-07-21 20:18:06 +0200

    fix DI bridge

--- src/js/Nittro/Extras/DropZone/Bridges/DropZoneDI.js
+++ src/js/Nittro/Extras/DropZone/Bridges/DropZoneDI.js
@@ -1,11 +1,11 @@
 _context.invoke(function() {
-    
+
     var DropZoneDI = _context.extend('Nittro.DI.BuilderExtension', function(containerBuilder, config) {
         DropZoneDI.Super.call(containerBuilder, config);
     }, {
         load: function() {
-            this._getContainerBuilder().addFactory('dropZone', 'Nittro.Widgets.DropZone::create()');
+            this._getContainerBuilder().addFactory('dropZone', 'Nittro.Extras.DropZone.DropZone::create()');
         }
     });
-    
+
 });
