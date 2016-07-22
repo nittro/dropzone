351e660c80cf46758f46db2ac4d429397ed670bd
danik <me@subsonic.cz>
2016-07-22 19:51:10 +0200

    fix parent constructor call in DI bridge

--- src/js/Nittro/Extras/DropZone/Bridges/DropZoneDI.js
+++ src/js/Nittro/Extras/DropZone/Bridges/DropZoneDI.js
@@ -5,7 +5,7 @@ _context.invoke('Nittro.Extras.DropZone.Bridges', function(Nittro) {
     }
 
     var DropZoneDI = _context.extend('Nittro.DI.BuilderExtension', function(containerBuilder, config) {
-        DropZoneDI.Super.call(containerBuilder, config);
+        DropZoneDI.Super.call(this, containerBuilder, config);
     }, {
         load: function() {
             this._getContainerBuilder().addFactory('dropZone', 'Nittro.Extras.DropZone.DropZone::create()');
